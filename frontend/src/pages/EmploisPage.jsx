d5d983adab5f500f0a250b0c28da08c9971a327f
sahuiejuleschristjunior-rgb <sahuiejuleschristjunior@gmail.com>
2025-12-18 07:08:43 +0800

    Add left menu to job feed page

--- frontend/src/pages/EmploisPage.jsx
+++ frontend/src/pages/EmploisPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import LeftMenuDesktop from "../components/LeftMenuDesktop";
 import JobFeed from "../components/JobFeed";
 
 export default function EmploisPage() {
-    return (
+  return (
+    <div className="jobfeed-page">
+      <aside className="jobfeed-left">
+        <LeftMenuDesktop />
+      </aside>
+
+      <main className="jobfeed-main">
         <div className="fb-center-column">
-            <JobFeed />
+          <JobFeed />
         </div>
-    );
+      </main>
+    </div>
+  );
 }
