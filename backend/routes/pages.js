743bc2b15603c8e476c15a74d86c2411bbbc3c82
sahuiejuleschristjunior-rgb <sahuiejuleschristjunior@gmail.com>
2025-12-21 21:25:15 +0800

    Increase upload limits to 500MB

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -41,7 +41,7 @@ function fileFilter(req, file, cb) {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 100 * 1024 * 1024 },
+  limits: { fileSize: 500 * 1024 * 1024 },
 });
 
 router.post("/", auth, ctrl.createPage);
