6cc0abefcdfb70118082b98437df4ca238aa9d97
EmploisFacile <contact@emploisfacile.org>
2025-12-29 22:30:07 +0100

    B

--- frontend/android/app/src/main/java/org/emploisfacile/app/MainActivity.java
+++ frontend/android/app/src/main/java/org/emploisfacile/app/MainActivity.java
@@ -1,30 +1,5 @@
 package org.emploisfacile.app;
 
-import android.os.Bundle;
-
-import androidx.core.view.WindowCompat;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        /*
-         🔥 FIX DÉFINITIF SAMSUNG / ANDROID
-         Empêche le WebView de dessiner sous :
-         - la status bar (haut)
-         - la navigation bar (bas)
-        */
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
-
-        /*
-         🎨 Optionnel mais recommandé :
-         Couleur cohérente avec ton thème sombre
-        */
-        getWindow().setStatusBarColor(0xFF020617);
-        getWindow().setNavigationBarColor(0xFF020617) ;
-    }
-}
+public class MainActivity extends BridgeActivity {}
